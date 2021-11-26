da0639ef8e6572133b322e367a48a3eac1a1a5fe
Lukas Gonzales <karmalor@yahoo.com>
2021-11-26 12:05:32 -0600

    Initial Commit

---
+++ Stretch+Paws2/Stretch_Paws2App.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_Paws2App.swift
+//  Stretch+Paws2
+//
+//  Created by Lukas Gonzales on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_Paws2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
